bacc80f4ce6284b81b67b4a77974ef50506d28df
NestaNdereva23 <nestandereva@gmail.com>
2024-06-08 16:18:37 +0300

    update 4.1

--- TrackMyCash/accounts/forms.py
+++ TrackMyCash/accounts/forms.py
@@ -4,19 +4,21 @@ from django.contrib.auth.models import User
 from .models import Expenses
 from django.forms import ModelForm, DateTimeInput
 
-
-#userlogin form
-
+# User login form
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2' ]
 
-
 class ExpenseForm(ModelForm):
     class Meta:
         model = Expenses
-        fields = ['account', 'expense_category', 'expense_amount', 'description', 'date_added' ]
+        fields = ['transaction_type', 'account', 'expense_category', 'expense_amount', 'description', 'date_added' ]
         widgets = {
-            'date_added': DateTimeInput(attrs={'type': 'datetime-local'}),
+            'transaction_type': forms.Select(attrs={'class': 'form-control equal-width'}),
+            'account': forms.Select(attrs={'class': 'form-control equal-width'}),
+            'expense_category': forms.Select(attrs={'class': 'form-control equal-width'}),
+            'expense_amount': forms.NumberInput(attrs={'class': 'form-control equal-width'}),
+            'description': forms.TextInput(attrs={'class': 'form-control equal-width'}),
+            'date_added': DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control equal-width'}),
         }
--- TrackMyCash/accounts/models.py
+++ TrackMyCash/accounts/models.py
@@ -13,19 +13,35 @@ class Expenses(models.Model):
         ("Transport", "Transport"),
         ("WiFi", "WiFi"),
     ]
+    TRANSACTION_TYPE = [
+        ("Expense", "Expense"),
+        ("Income", "Income"),
+        ("Transfer", "Transfer"),
+
+    ]
 
     ACCOUNT_CATEGORY = [
         ("Mpesa", "Mpesa"),
         ("Cash", "Cash"),
         ("Bank", "Bank"),
+        ("Crypto", "Crypto"),
+
     ]
 
+    transaction_type = models.CharField(max_length=15, choices=TRANSACTION_TYPE, default="Expense")
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     account = models.CharField(max_length=15, choices=ACCOUNT_CATEGORY, default="Cash")
     expense_category = models.CharField(max_length=15, choices=EXPENSE_CATEGORY, default="Car")
     description = models.CharField(max_length=1000, blank=True, null=True)
     expense_amount = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateTimeField(default=timezone.now)
+
+    #ordering transactions
+    class Meta:
+        ordering = ['-date_added']
+        # indexes = [models.Index(fields=['-date_added']), ]
+
+
      
    
     # def __str__(self):
