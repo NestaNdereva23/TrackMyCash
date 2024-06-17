eb5b610eae6c746ba78c3636ce4e57de1f23c002
NestaNdereva23 <nestandereva@gmail.com>
2024-06-17 14:22:01 +0300

    update 1.1.3

--- TrackMyCash/TrackMyCash/settings.py
+++ TrackMyCash/TrackMyCash/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
